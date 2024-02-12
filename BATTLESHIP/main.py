9cfb079a753febc5efad040cc0d341f607ed7099
Jacksons20kpr <blondespeeder@outlook.com>
2024-02-12 10:46:41 -0500

    k

---
+++ BATTLESHIP/main.py
@@ -0,0 +1,10 @@
+#Made by: Jackson Shangraw
+#Date: February 7th 2024
+#Purpose: 1 Player Battleship
+
+squares=["1a","1b","1c","1d","1e","2a","2b","2c","2d","2e","3a","3b","3c","3d","3e""4a","4b","4c","4d","4e","5a","5b","5c","5d","5e"]
+squares=["□","□","□","□","□","□","□","□","□","□","□","□","□","□","□","□","□","□","□","□","□","□","□","□","□"]
+ships=["Ship1","Ship2","Ship3"]
+hit=["3c","3d","5a","5b","5c","1e","2a"]
+hit=["☒","☒","☒"]
+print(hit)
--- main.py
+++
@@ -1,4 +0,0 @@
-#Made by: Jackson Shangraw
-#Date: February 7th 2024
-#Purpose: This is a demo on how to make headers
-
