a461a880408726f2d1861ecb99a68f4121e2cf14
Jacksons20kpr <blondespeeder@outlook.com>
2024-03-07 11:01:47 -0500

    Oh YEEEEEAHHHHH

--- 2d_list/main.py
+++ 2d_list/main.py
@@ -1,6 +1,21 @@
 #Name: Jackson
 #Date: 01/03/24
 #Purpose: 2D List
-grades= [["Ethan",80],["John",60],["Dylan",75],[95],[85]]
-#def student_average():
-    
+grades= [["Ethan", 80,77,88,99],
+         ["John", 60,88],
+         ["Dylan", 75,21],
+         ["Thomas", 95,99],
+         ["Jerald", 85,88]]
+def class_average(course):
+    total=0
+    for student in grades:
+        total+=student[course]
+    return total/len(grades)
+
+def student_average(student):
+    total=0
+    total=grades[student][1]+grades[student][2]+grades[student][3]+grades[student][4]
+    return total/4
+
+print(class_average(2))
+print(student_average(0))     
